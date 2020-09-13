b5e9a4a17601f58358b6cfa0bf93b30bb7fa6697
YoheiIguchi <matthew.at.muse@gmail.com>
2020-09-13 22:17:12 +0900

    obj prefix

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -29,7 +29,7 @@ impl Eval for Statement {
   }
 }
 
-impl Eval for Expression {
+impl Eval for &Expression {
   fn eval(&self) -> Option<Object> {
     match self {
       Expression::IntegerExpr(i) => {
@@ -40,8 +40,37 @@ impl Eval for Expression {
         TokenType::False => Some(Object::BooleanObj(false)),
         _ => None,
       },
+      Expression::PrefixExp {
+        token,
+        operator,
+        right,
+      } => {
+        let exp = right.as_ref();
+        let exp = exp.eval().unwrap();
+        eval_prefix_expression(operator, exp)
+      }
       _ => None,
     }
+  }
+}
+
+fn eval_prefix_expression(operator: &str, right: Object) -> Option<Object> {
+  match operator {
+    "!" => Some(eval_bang_operator_expression(right)),
+    _ => None,
+  }
+}
+
+fn eval_bang_operator_expression(right: Object) -> Object {
+  match right {
+    Object::BooleanObj(b) => {
+      if b {
+        Object::BooleanObj(false)
+      } else {
+        Object::BooleanObj(true)
+      }
+    }
+    _ => Object::BooleanObj(false),
   }
 }
 
@@ -67,13 +96,33 @@ mod test {
     }
   }
 
+  #[test]
+  fn test_bang_operator() {
+    let tests = vec![
+      ("!true", false),
+      ("!false", true),
+      ("!5", false),
+      ("!!true", true),
+      ("!!false", false),
+      ("!!5", true),
+    ];
+
+    for tt in tests {
+      let evaluated = test_eval(tt.0.to_string()).unwrap();
+      assert!(test_boolean_object(&evaluated, tt.1))
+    }
+  }
+
   fn test_eval(input: String) -> Option<Object> {
     let l = Lexer::new(input);
     let mut p = Parser::new(l);
     let program = p.parse_program();
 
     match program.eval() {
-      Some(s) => Some(s),
+      Some(s) => {
+        dbg!(&s);
+        Some(s)
+      }
       _ => None,
     }
   }
